beafce6f2829fa68c9d70ca3a82ab2659dad90de
SennaLippens <36761183+SennaLippens@users.noreply.github.com>
2019-09-03 15:39:55 +0200

    python3 opdr. 1
    dit is opdracht 1 van python 3

---
+++ python3 opdr.1.py
@@ -0,0 +1,7 @@
+bedrijven = {"Apple": "Steve Jobs", "Microsoft": "Bill Gates"}
+print("hier komt de maken van apple")
+print(bedrijven["Apple"])
+print("hier komt de maker van apple")
+print(bedrijven["Microsoft"])
+
+print(bedrijven.keys())
