7e7883ad323c49c9701d1040c3deefaecc93776a
SennaLippens <36761183+SennaLippens@users.noreply.github.com>
2019-09-20 12:50:54 +0200

    iets???????

---
+++ python3 lesbrief 2 functies en arrays.py
@@ -0,0 +1,47 @@
+print("hier komen de opdrachten bij functies en arrays")
+print("")
+
+print("opdracht 1")
+def reverse(array):
+    array.reverse()
+    return array
+print(reverse(["hoi", "hey", "jo"]))
+
+
+print("")
+print("opdracht 2")
+lijst = ("heyy", "hoiii", "heyy", "hoiii")
+teller = 0
+def len(lijst, teller):
+    for l in lijst:
+        teller = teller + 1
+    print(teller)
+len(lijst, teller)
+
+
+print("")
+print("opdracht 3")
+def opdr3(aaa, bbb):
+    aaa.append(bbb)
+    return aaa
+print(opdr3(["hehe", "woord"], "eee"))
+
+
+print("")
+print("opdracht 4")
+def opdr4(fff, ggg):
+    fff.insert(1,ggg)
+    return fff
+print(opdr4(["hhh","jjj"], "iii"))
+
+
+print("")
+print("opdracht 5")
+def poep():
+    
+
+
+print("")
+print("opdracht 6")
+
+
